f8a429dec62840c00d651642a303f27b9c202e12
cationic <kangmg0827@gmail.com>
2024-11-14 17:12:46 +0000

    [update] make installable

--- setup.py
+++ setup.py
@@ -9,6 +9,27 @@ setup(
         "xequinet.utils.basis": ["*.dat"],  # basis folder data
         "xequinet.utils.pre_computed": ["*.pt"],  # pre_computed folder data
     },
+    install_requires=[
+        'torch>=2.0', 
+        'torch-geometric>=2.0',
+        'torch-cluster',
+        'torch-scatter',
+        'pytorch-warmup>=0.1',
+        'e3nn>=0.5',
+        'pydantic>=2.6',
+        'ase>=3.22',
+        #==== Extra requirements ====#
+        #--------- Geo. Opt. --------#
+        # geometric>=1.0
+        # pyscf>=2.4
+        #----------- PIMD -----------#
+        # ipi>=2.6
+        #------ Delta Learning ------#
+        # tblite>=0.3 # via conda
+        # tblite-python>=0.3 # via conda
+        # ===========================#
+    ],
+    python_required='>=3.9',
     entry_points={
         'console_scripts': [
             "xeqtrain = xequinet.run.train:main",
